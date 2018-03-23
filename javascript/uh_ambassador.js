b305e14b160be53819761d4ead67ebe1348dd7e1
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-03-23 18:51:17 +0200

    Revert "a links included in the all the navtabs topright"
    mobile not perfectly done

--- javascript/uh_ambassador.js
+++ javascript/uh_ambassador.js
@@ -1,11 +1,11 @@
 function openSlideNav() {
-    document.getElementById("side-menu").style.width = "10rem";
-    document.getElementById("side-menu").style.height = "20rem";
-    document.getElementById("main-test").style.marginLeft = "12rem";
+    document.getElementById("side-nav").style.width = "10rem";
+    document.getElementById("side-nav").style.height = "20rem";
+    document.getElementById("main").style.marginLeft = "12rem";
 }
 
 // for closing the slide navigation bar
 function closeSlideNav() {
-    document.getElementById("side-menu").style.width = "0";
-    document.getElementById("main-test").style.marginLeft = "0";
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
